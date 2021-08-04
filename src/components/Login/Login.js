f8d6a47a5ca57c0d469f6bbf2200a05e3b685bc4
hamza <hamza.benkhoud@etudiant-isi.utm.tn>
2021-08-04 19:00:28 +0100

    redirect url

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -2,7 +2,7 @@ import './Login.css'
 
 export default function Login() {
   function loginHandler() {
-    const authUrl = `https://accounts.spotify.com/authorize?client_id=${process.env.REACT_APP_API_KEY_SPOTIFY}&response_type=token&scope=playlist-modify-public&redirect_uri=http://localhost:3000/`
+    const authUrl = `https://accounts.spotify.com/authorize?client_id=${process.env.REACT_APP_API_KEY_SPOTIFY}&response_type=token&scope=playlist-modify-public&redirect_uri=https://spotify-playlists-api.surge.sh/`
     window.location = authUrl
   }
   return (
